6cf82a9c6fbd075ca54390e9a97642ef29e165da
dhrithi <dhriti280@gmail.com>
2025-01-15 20:44:53 -0800

    fixed navbar issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingpageComponent
   ],
   imports: [
     BrowserModule,
